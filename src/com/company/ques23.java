b10d6e8bc95eb3c12c9136c1f920a3ab784e3192
Sanyam Jain <jainsanyam0306@gmail.com>
2020-05-28 23:52:38 +0530

    Initial commit

---
+++ src/com/company/ques23.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ques23 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/ques25.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.Scanner;
+
+public class ques25 {
+    public static void main(String[] args) {
+
+    }
+}
